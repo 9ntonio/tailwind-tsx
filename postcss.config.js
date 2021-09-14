602508bc0341537fb611dfc85f61734b6caf5bdf
Antonio Almena <aalmena1@gmail.com>
2021-09-14 12:42:19 -0700

    [9ntonio][Feature: adding cssnano]

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require("cssnano");
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
-  }
+  plugins: [
+    require("tailwindcss"),
+    // process.env.NODE_ENV === "production" ? require("autoprefixer") : null,
+    // process.env.NODE_ENV === "production"
+    //   ? cssnano({ preset: "default" })
+    //   : null,
+    require("autoprefixer"),
+    cssnano({ preset: "default" }),
+    purgecss({
+      content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    })
+  ]
 };
